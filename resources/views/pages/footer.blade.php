73f179962251515fec7d972df14c68d1cc157563
SarfraazAhmad <sa4201601@gmail.com>
2024-10-01 11:32:42 +0500

    link the style file and use {{asset()}}

--- resources/views/pages/footer.blade.php
+++ resources/views/pages/footer.blade.php
@@ -9,28 +9,10 @@
 @empty
     ('not found');
 @endforelse --}}
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <style>
-        /* Style the footer */
-footer {
-  background-color: #777;
-  padding: 10px;
-  text-align: center;
-  color: white;
-}
-    </style>
-</head>
-<body>
+    
     <footer>
         <p>Footer</p>
       </footer>
-</body>
-</html>
+
 
 
